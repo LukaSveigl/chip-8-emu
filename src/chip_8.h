113c16ea0a9f01bc334c22929c34d99712cd7565
LukaSveigl <lukasveigl2708@gmail.com>
2026-01-19 21:33:56 +0100

    Doxygen comments
    Added Doxygen comments to the functions in the chip 8 header file,
    describing the structure and functions.

--- src/chip_8.h
+++ src/chip_8.h
@@ -12,6 +12,12 @@
 #define FONTSET_SIZE  80
 #define MAX_FILE_SIZE MEMORY_SIZE - 512
 
+/**
+ * The CHIP-8 hardware structure.
+ *
+ * This structure contains all the necessary elements to emulate
+ * the architecture of the systems on which CHIP-8 can run.
+ */
 typedef struct chip_8 {
     uint8_t _memory[MEMORY_SIZE];
     uint8_t _V[REGISTERS];
@@ -30,10 +36,28 @@ typedef struct chip_8 {
     uint8_t _keymap[KEYMAP_SIZE];
 } chip_8;
 
+/**
+ * Initializes the CHIP-8 structure by setting all of the memory fields to
+ * zero, initializing the fontset and setting the program counter to 0x200.
+ *
+ * @param emu The emulator structure.
+ */
 void chip_8_init(chip_8 *emu);
 
+/**
+ * Loads the ROM at the given path into the memory of the emulator.
+ *
+ * @param emu  The emulator structure.
+ * @param path The path to the ROM.
+ * @return True if the ROM is loaded successfully, False otherwise.
+ */
 bool chip_8_load(chip_8 *emu, const char *path);
 
+/**
+ * Emulates one cycle of the program, processing a single opcode.
+ *
+ * @param emu The emulator structure.
+ */
 void chip_8_emulate_cycle(chip_8 *emu);
 
 #endif // CHIP_8_H
